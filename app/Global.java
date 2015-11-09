237507287abc1845b005a5e7b2650c789713780b
Carlos <cmoro@deusto.es>
2015-11-09 13:19:58 +0100

    Global

---
+++ app/Global.java
@@ -0,0 +1,15 @@
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+
+
+public class Global extends GlobalSettings {
+
+    public void onStart(Application app) {
+        Logger.info("Application has started!");
+    }
+
+    public void onStop(Application app) {
+        Logger.info("Applicatino has stopped!");
+    }
+}
