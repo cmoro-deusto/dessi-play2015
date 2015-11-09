0ff52e36eab60949f111d590c750d35cd270365c
Carlos <cmoro@deusto.es>
2015-11-09 12:41:27 +0100

    Bucle y condicional en view

--- app/controllers/HelloWorld.java
+++ app/controllers/HelloWorld.java
@@ -3,11 +3,21 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HelloWorld extends Controller {
 
     public Result index(String name, int age) {
         String location = Play.application().configuration().getString("location");
-        return ok(views.html.hello.render(name, age, location));
+        List<String> array = new ArrayList<>();
+        array.add("Elemento 1");
+        array.add("Elemento 2");
+        array.add("Elemento 3");
+        array.add("Elemento 4");
+        array.add("Elemento 5");
+
+        return ok(views.html.hello.render(name, age, location, array));
     }
 }
