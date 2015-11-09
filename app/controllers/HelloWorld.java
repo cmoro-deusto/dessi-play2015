1dafb45b53c10b1b28901dcbcbe634e659e0eca0
Carlos <cmoro@deusto.es>
2015-11-09 11:05:09 +0100

    Add view

--- app/controllers/HelloWorld.java
+++ app/controllers/HelloWorld.java
@@ -7,6 +7,6 @@ import play.mvc.*;
 public class HelloWorld extends Controller {
 
     public Result index(String name, int age) {
-        return ok("Hello " + name + " you are " + age + " years old.");
+        return ok(views.html.hello.render(name, age));
     }
 }
