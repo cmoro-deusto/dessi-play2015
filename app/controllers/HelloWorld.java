0f694372e41f1ef39e32d85e6bc0a33f1c00668c
Carlos <cmoro@deusto.es>
2015-11-09 10:56:57 +0100

    Add Typed Param

--- app/controllers/HelloWorld.java
+++ app/controllers/HelloWorld.java
@@ -6,7 +6,7 @@ import play.mvc.*;
 
 public class HelloWorld extends Controller {
 
-    public Result index(String name) {
-        return ok("Hello " + name);
+    public Result index(String name, int age) {
+        return ok("Hello " + name + " you are " + age + " years old.");
     }
 }
