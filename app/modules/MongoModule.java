8e59ab04f47ade71c42eecfbdb8ae9729e172dd6
Carlos <cmoro@deusto.es>
2015-11-13 17:25:24 +0100

    Fix classes names

--- app/modules/MongoModule.java
+++ app/modules/MongoModule.java
@@ -1,6 +1,12 @@
 package modules;
 
+import com.google.inject.Inject;
 import com.mongodb.MongoClient;
+
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
 import play.Logger;
 
 import javax.inject.Singleton;
@@ -8,12 +14,27 @@ import javax.inject.Singleton;
 @Singleton
 public class MongoModule {
 
-    public static MongoClient mongo;
+    public MongoClient mongo;
+    public Morphia morphia;
+    public Datastore ds;
 
-    public MongoModule() {
+
+    @Inject
+    public MongoModule(ConfigModule config) {
 
         Logger.info("Connecting to mongo...");
-        this.mongo = new MongoClient();
 
+        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(config.conns).build();
+        ServerAddress server = new ServerAddress(config.host, config.port);
+        mongo = new MongoClient(server, options);
+        Logger.info("Connected to Mongo at " + server.toString());
+
+        Logger.info("Starting Morphia Datastore...");
+        morphia = new Morphia();
+        ds = morphia.createDatastore(mongo, config.db);
+
+        Logger.info("Datastore: " + config.db);
+        ds.ensureIndexes();
+        ds.ensureCaps();
     }
 }
